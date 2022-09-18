fd208c7ec2a02c7e996a2e030a8eedf919dc2b56
AntonRusev <anton.rusev.code@gmail.com>
2022-09-18 12:00:52 +0300

    feat: add record functions

--- Project/contracts/PaymentRegistry.sol
+++ Project/contracts/PaymentRegistry.sol
@@ -9,11 +9,26 @@ contract PaymentRegistry {
 
     // project address => user => role
     mapping(address => mapping(address => Role)) public getRole;
+    // project address => document ID => record
+    mapping(address => mapping(uint => Record)) public getRecord;
+    // document ID => project
+    mapping(uint => address) public getProjectByDocumentId;
 
     struct Role {
         bool isOwner;
         bool isAdmin;
     }
+
+    struct Record {
+        uint documentId;
+        uint amount;
+        bool exists;
+        RecordType recordType;
+        RecordStatus recordStatus;
+    }
+
+    enum RecordType { DISBURSE, CANCEL }
+    enum RecordStatus { CONFIRMED, CANCELED, CREATED }
 
     modifier onlyAdmin(address _project) {
         require(getRole[_project][msg.sender].isAdmin == true, "Growr. - access denied");
@@ -31,9 +46,17 @@ contract PaymentRegistry {
     }
 
     event RoleUpdated(address indexed projectAddress, address indexed user, Role updatedRole);
+    event RecordCreated(address indexed projectAddress, uint indexed documentId, Record record);
 
     constructor() {
         factory = msg.sender;
+    }
+
+    function requireOnlyAdmin(address _project) internal onlyAdmin(_project) {}
+    function requireDocumentToExists(bool _exists, address _project, uint _documentId) internal view {
+        string memory errorMessage = _exists ? "Growr. - record does not exists" : "Growr. - record already exists";
+
+    	require(getRecord[_project][_documentId].exists == _exists, errorMessage);
     }
 
     function addOwner(address _project, address _user) public onlyFactory {
@@ -54,5 +77,32 @@ contract PaymentRegistry {
         getRole[_project][_user] = updatedRole;
         
         emit RoleUpdated(_project, _user, updatedRole);
+    }
+
+    function createRecord(address _project, uint _documentId, uint _amount, RecordType _type) external onlyAdmin(_project) {
+        require(_project != address(0), "Growr. - project is zero address");
+        require(_amount > 0, "Growr. - amount is too low");
+
+        requireDocumentToExists(false, _project, _documentId);
+
+        Record memory _record = getRecord[_project][_documentId];
+
+        _record = Record(_documentId, _amount, true, _type, RecordStatus.CREATED);
+
+        getRecord[_project][_documentId] = _record;
+        getProjectByDocumentId[_documentId] = _project;
+
+        emit RecordCreated(_project, _documentId, _record);
+    }
+
+    function confirmRecord(uint _documentId) external {
+        address _project = getProjectByDocumentId[_documentId];
+
+        requireOnlyAdmin(_project);
+        requireDocumentToExists(true, _project, _documentId);
+
+        Record storage _record = getRecord[_project][_documentId];
+
+        _record.recordStatus = RecordStatus.CONFIRMED;
     }
 }
